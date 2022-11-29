885b354108d6ce076e54c0fcc70c9c0a003d4f18
s-121-24 <s-121-24@gumiforest.ae.krakow.pl>
2022-11-29 12:59:19 +0100

    29.11 CW

---
+++ 07-AggregationAndComposition/Book.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class Book {
+    private int yearOfCreation;
+    private String content;
+
+    public int getYearOfCreation() {
+        return yearOfCreation;
+    }
+
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    private int numberOfPages;
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    ArrayList<String> categories = new ArrayList<String>();
+
+    public ArrayList<String> getCategories() {
+        return categories;
+    }
+
+    public Book(int yearOfCreation, String title, int numberOfPages, String[] categories, String content) {
+        this.yearOfCreation = yearOfCreation;
+        this.title = title;
+        this.content = content;
+        this.numberOfPages = numberOfPages;
+        for (int id = 0; id < categories.length; id++) {
+            addCategory(categories[id]);
+        }
+
+    }
+
+    public void addCategory(String category) {
+        categories.add(category);
+    }
+
+    public String readBook() {
+        return content;
+    }
+
+}
